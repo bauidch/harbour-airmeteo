1d6cde053c49093068b6731bbda6c17f7ab5f79e
Lukas Bauer <info@bauid.ch>
2020-10-19 15:59:16 +0200

    Update client.js

--- qml/js/client.js
+++ qml/js/client.js
@@ -5,39 +5,15 @@ function httpGet(url) {
     return xmlHttp;
 }
 
-function get_location_length() {
-    var locationsRAW = httpGet("https://themachine.jeremystucki.com/coop/api/v2/locations")
+function get_metar() {
+    var locationsRAW = httpGet("https://www.aviationweather.gov/adds/dataserver_current/httpparam?dataSource=metars&requestType=retrieve&stationString=LSZH&hoursBeforeNow=1&format=xml&mostRecent=true")
 
-    try {
-        var locations = JSON.parse(locationsRAW.responseText);
-    } catch (e) {
-        console.log("error: failed to parse week overview json");
-    }
+    console.log(locationsRAW)
     if (locationsRAW.status >= 200 && locationsRAW.status < 400) {
-        return locations.results.length;
-    } else {
-        console.log('error or no locations')
-    }
-}
-
-function get_all_locations() {
-    var locationsRAW = httpGet("https://themachine.jeremystucki.com/coop/api/v2/locations")
-
-    try {
-        var locations = JSON.parse(locationsRAW.responseText);
-    } catch (e) {
-        console.log("error: failed to parse json");
-    }
-    if (locationsRAW.status >= 200 && locationsRAW.status < 400) {
-
-        var ret = [];
-        for (var i = 0; i < locations.results.length; i++) {
-            var cordi = locations.results[i].coordinates.coordinates
-            ret.push({"name":locations.results[i].name, "id":locations.results[i].id, "zip":locations.results[i].address.zip, "city":locations.results[i].address.city, "longitude":cordi[0], "latitude":cordi[1]});
-        }
-
-        return ret;
-
+        console.log(locationsRAW)
+        var domParser = new DOMParser();
+        var xmlDocument = domParser.parseFromString(locationsRAW.responseText, "text/xml");
+        console.log(xmlDocument)
     } else {
         console.log('error or no locations')
     }
